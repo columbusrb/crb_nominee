014d1f023cd043fbeb300ee4889e9a22fb006999
Matt Darby <matt.darby@rackspace.com>
2016-12-03 18:40:53 -0500

    Heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule CrbNominee.UserSocket do
   # channel "room:*", CrbNominee.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
