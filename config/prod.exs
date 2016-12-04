b4ca9c45d7e48e4d80b23c5bbd5d379f5b03e6f6
Matt Darby <matt.darby@rackspace.com>
2016-12-03 21:23:55 -0500

    REbuild

--- config/prod.exs
+++ config/prod.exs
@@ -27,6 +27,7 @@ config :crb_nominee, CrbNominee.Repo,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
