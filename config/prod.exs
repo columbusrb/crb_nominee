3cb20403851d52945af7111d31a6673781e7f587
Jon <jon@joncanady.com>
2019-11-20 13:31:17 -0500

    Update prod URL for assets.

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :crb_nominee, CrbNomineeWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "murmuring-fjord-61477.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "crb-candidates.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
